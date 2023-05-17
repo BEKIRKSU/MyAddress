e88ea4ff4eb419a76f6720b608b05aeeaf6a052b
BEKIRKSU <bekirkscu@gmail.com>
2023-05-17 07:51:03 -0700

    HOME

--- src/componets/home/home.jsx
+++ src/componets/home/home.jsx
@@ -43,7 +43,7 @@ function Home() {
         <section className="content-section-1">
           <div className="content-section-1-punchline"></div>
           <div className="body-content">
-            <h1>How many people/businesses have my address?</h1>
+            <h1>A Revolutionary, Safe and Private strucutre to deliveries. There are over 450million deliveries per year. </h1>
           </div>
         </section>
       </main>
